77ea4eb74fdc62acc9a58c0f95116aefc8ee28fb
Wolfpack075 <kazifahimtahmid@gmail.com>
2023-11-27 19:13:19 +0600

    Created NetworkManager to handle URL calls and created seperate Services for Coin and CoinImage data fetch

--- To Do List App/Services/CoinDataService.swift
+++ To Do List App/Services/CoinDataService.swift
@@ -2,7 +2,32 @@
 //  CoinDataService.swift
 //  To Do List App
 //
-//  Created by Shoumik Barman Polok on 27/11/23.
+//  Created by Kazi Fahim Tahmid on 27/11/23.
 //
 
 import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+//    var cancellables = Set<AnyCancellable>()
+    
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins()
+    {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en") else { return }
+        coinSubscription = NetworkingManager.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            })
+//            .store(in: &cancellables)
+    }}
