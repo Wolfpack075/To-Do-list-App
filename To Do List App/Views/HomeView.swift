e88d359125337472ed2690e2c8b988c09d39fda2
Wolfpack075 <kazifahimtahmid@gmail.com>
2023-11-28 07:15:18 +0600

    MarketCap View integrated with main view

--- To Do List App/Views/HomeView.swift
+++ To Do List App/Views/HomeView.swift
@@ -20,6 +20,7 @@ struct HomeView: View {
             VStack {
                 
                 homeHeader
+                HomeStatsView(showPortfolio: $showPortfolio)
                 
                 SearchBarView(searchText: $vm.searchText)
                 columnTitles
