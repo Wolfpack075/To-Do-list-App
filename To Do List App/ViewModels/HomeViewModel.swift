2301ca7372eab1ad1fb0569320cff40339538bd7
Wolfpack075 <kazifahimtahmid@gmail.com>
2023-11-28 07:05:38 +0600

    Implemented Search functionalities and added Model for Market Stats

--- To Do List App/ViewModels/HomeViewModel.swift
+++ To Do List App/ViewModels/HomeViewModel.swift
@@ -10,6 +10,15 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
+//    @Published var statistics: [StatisticModel] = [
+//
+//    StatisticModel(title: "Title", value: "Value" , percentageChange: 1),
+//    StatisticModel(title: "Title", value: "Value"),
+//    StatisticModel(title: "Title", value: "Value"),
+//    StatisticModel(title: "Title", value: "Value" , percentageChange: -7)
+//
+//    ]
+    
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
@@ -22,10 +31,32 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+//        dataService.$allCoins
+//            .sink { [weak self] (returnedCoins) in
+//                self?.allCoins = returnedCoins
+//            }
+//            .store(in: &cancellables)
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
+                
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String , coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+                return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        return coins.filter { (coin) in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText)
+        }
     }
 }
